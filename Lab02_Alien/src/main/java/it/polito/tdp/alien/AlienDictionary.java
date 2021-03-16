41b50042576e3d9baae28da8aacb443be9b6dcb8
Manuel Taglieri <s257476@studenti.polito.it>
2021-03-16 19:50:03 +0100

    Esercizio 1

---
+++ Lab02_Alien/src/main/java/it/polito/tdp/alien/AlienDictionary.java
@@ -0,0 +1,26 @@
+package it.polito.tdp.alien;
+
+import java.util.*;
+
+public class AlienDictionary {
+	
+	private LinkedHashMap<String, Word> parole;
+	
+	public AlienDictionary() {
+		this.parole = new LinkedHashMap<>();
+	}
+	
+	public void addWord(String alienWord, String translation) {
+		if (parole.get(alienWord)==null) {
+			parole.put(alienWord, new Word(alienWord, translation));
+		}
+		else {
+			parole.get(alienWord).setTranslation(translation);
+		}
+	}
+	
+	public String translateWord(String alienWord) {
+		return parole.get(alienWord).getTranslation();
+	}
+
+}
--- Lab02_Alien/src/main/java/it/polito/tdp/alien/FXMLController.java
+++ Lab02_Alien/src/main/java/it/polito/tdp/alien/FXMLController.java
@@ -2,10 +2,11 @@ package it.polito.tdp.alien;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
 
@@ -14,27 +15,67 @@ public class FXMLController {
 
     @FXML
     private URL location;
-    
+
+    @FXML
+    private TextField txtParola;
+
     @FXML
     private Button btnTranslate;
-    
+
+    @FXML
+    private TextArea txtResult;
+
     @FXML
     private Button btnReset;
+    
+    private AlienDictionary dizionario;
+    private boolean pieno;
+
+    @FXML
+    void doReset(ActionEvent event) {
+    	
+    	txtParola.clear();
+    	txtResult.clear();
+
+    }
 
     @FXML
     void doTranslate(ActionEvent event) {
-    	// TODO - add the button and complete this    	
+    	
+    	String parola = txtParola.getText();
+
+    	if (parola.matches("[a-zA-Z\s]")) {
+    		String[] parole = parola.split(" ");
+    		if (parole.length==1 && pieno) {
+    			String traduzione = dizionario.translateWord(parole[0]);
+    			txtResult.appendText("La traduzione della parola " + parole[0] + " é " + traduzione + ".\n");
+    		}
+    		else if (parole.length==2) {
+    			dizionario.addWord(parole[0], parole[1]);
+    			txtResult.appendText("La parola " + parole[0] + " é stata correttamente inserita nel dizionario con il significato di " + parole[1] +".\n");
+    			this.pieno = true;
+    		}
+    		else if (!pieno) {
+    			txtResult.appendText("Inserisci almeno una parola in dizionario prima di poter tradurre.\n");
+    		}
+    		else {
+    			txtResult.appendText("ERRORE: inserisci al massimo 2 parole.\n");
+    		}
+    	}
+    	else {
+    		txtResult.appendText("ERRORE: consentiti solo caratteri di testo e spazi.\n");
+    		return;
+    	}
     }
-    
-    
-    @FXML
-    void doReset(ActionEvent event) {
-    	// TODO - add the button and complete this 
-    }
-    
-    
+
     @FXML
     void initialize() {
-
+        assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnTranslate != null : "fx:id=\"btnTranslate\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
+        
+        this.dizionario = new AlienDictionary();
+        this.pieno = false;
     }
 }
